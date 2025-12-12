4ea17ca467b66828a42c21763f85a36aba20a56a
Saurabh Singh <spsinght@amazon.com>
2025-12-12 00:28:01 -0800

    non blocking IO poc old commit into public fork.

--- server/src/main/java/org/opensearch/lucene/cache/NIOFSDirectoryWithODirect.java
+++ server/src/main/java/org/opensearch/lucene/cache/NIOFSDirectoryWithODirect.java
@@ -8,8 +8,10 @@ import org.apache.lucene.util.IOUtils;
 
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class NIOFSDirectoryWithODirect extends NIOFSDirectory {
 
@@ -21,13 +23,41 @@ public class NIOFSDirectoryWithODirect extends NIOFSDirectory {
     public NIOFSDirectoryWithODirect(Path path) throws IOException {
         super(path);
     }
+    static final OpenOption ExtendedOpenOption_DIRECT; // visible for test
+
+    static {
+        OpenOption option;
+        try {
+            final Class<? extends OpenOption> clazz =
+                Class.forName("com.sun.nio.file.ExtendedOpenOption").asSubclass(OpenOption.class);
+            option =
+                Arrays.stream(clazz.getEnumConstants())
+                    .filter(e -> e.toString().equalsIgnoreCase("DIRECT"))
+                    .findFirst()
+                    .orElse(null);
+        } catch (
+            @SuppressWarnings("unused")
+            Exception e) {
+            option = null;
+        }
+        ExtendedOpenOption_DIRECT = option;
+    }
+
+    private static OpenOption getDirectOpenOption() {
+        if (ExtendedOpenOption_DIRECT == null) {
+            throw new UnsupportedOperationException(
+                "com.sun.nio.file.ExtendedOpenOption.DIRECT is not available in the current JDK version.");
+        }
+        return ExtendedOpenOption_DIRECT;
+    }
+
 
     @Override
     public IndexInput openInput(String name, IOContext context) throws IOException {
         ensureOpen();
         ensureCanRead(name);
         Path path = getDirectory().resolve(name);
-        FileChannel fc = FileChannel.open(path, StandardOpenOption.READ);
+        FileChannel fc = FileChannel.open(path, StandardOpenOption.READ, getDirectOpenOption());
         boolean success = false;
         try {
             final NIOFSDirectIOIndexInput indexInput =
