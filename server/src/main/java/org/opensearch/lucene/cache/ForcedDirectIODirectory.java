0f7f8f19ced0311bfbb15a085a20899eb3551902
Saurabh Singh <spsinght@amazon.com>
2025-12-12 20:05:27 -0800

    non blocking IO poc old commit into public fork.

--- server/src/main/java/org/opensearch/lucene/cache/ForcedDirectIODirectory.java
+++ server/src/main/java/org/opensearch/lucene/cache/ForcedDirectIODirectory.java
@@ -29,6 +29,7 @@ public class ForcedDirectIODirectory extends DirectIODirectory {
 
     protected boolean useDirectIO(String name, IOContext context, OptionalLong fileLength) {
         if (fileLength.orElse(minBytesDirect) >= minBytesDirect) {
+            System.out.println("Using directIO for " + name + " size (mb) " + fileLength.orElse(minBytesDirect)/(1024*1024) );
             return true;
         }
        return false;
